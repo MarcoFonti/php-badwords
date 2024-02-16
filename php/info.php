2d5009efc2e09399a9b32b28ec6ac307791ce511
MarcoFonti <theemporiumg@gmail.com>
2024-02-16 16:55:55 +0100

    add text length

--- php/info.php
+++ php/info.php
@@ -1,3 +1,22 @@
 <?php
 
-echo 'Ciao';
+$text = $_GET['text'];
+
+$text_length = strlen($text);
+
+var_dump($text_length)
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Le tue Info</title>
+</head>
+<body>
+    <p>Hai scritto: <?= $text ?></p>
+    <p>Lunghezza testo: <?= $text_length ?></p>
+</body>
+</html>
