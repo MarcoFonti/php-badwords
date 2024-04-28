9d54e6ed9ae8eee2150331b34db3045158597d05
MarcoFonti <theemporiumg@gmail.com>
2024-04-28 13:56:42 +0200

    add commit

--- php/info.php
+++ php/info.php
@@ -1,10 +1,17 @@
 <?php
 
+/* RECUPERO IL VALORE INSERITO DALL'UTENTE */
 $text = $_GET['text'];
 
+
+/* CALCOLO LA LUNGHEZZA DEL TESTO */
 $text_length = strlen($text);
 
-$text_censored = str_replace('ciao', '***', $text);
+
+/* CESURO IL TESTO INVIATO DALL'UTENTE */
+$text_censored = str_replace($text, '***', $text);
+
+
 
 var_dump($text_censored);
 
@@ -12,14 +19,17 @@ var_dump($text_censored);
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Le tue Info</title>
 </head>
+
 <body>
     <p>Hai scritto: <?= $text ?></p>
     <p>Lunghezza testo: <?= $text_length ?></p>
     <p>Testo censurato: <?= $text_censored ?></p>
 </body>
+
 </html>
